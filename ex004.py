e43c21f5df2958e164a6222c2d1db6a24f33d323
Beatriz Rodrigues <beatrizrodrrigues@gmail.com>
2024-02-15 05:34:42 -0300

    Dissecando uma variável

---
+++ ex004.py
@@ -0,0 +1,10 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o eu tipo primitivo e todas as informações possíveis sobre ele.
+var = input('Digite algo: ')
+print('O tipo primitivo desse valor é', type(var))
+print('Só tem espaços?', var.isspace())
+print('É um número?', var.isdigit())
+print('É alfabético?', var.isalpha())
+print('É alfanumérico?', var.isalnum())
+print('Está em maiúsculas?', var.isupper())
+print('Está em minúsculas?', var.islower())
+print('Está capitalizada?', var.istitle())
