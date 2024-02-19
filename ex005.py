6eb8d8e5fecf261e9d331b0e27c250891cc29d87
Beatriz Rodrigues <beatrizrodrrigues@gmail.com>
2024-02-19 01:54:53 -0300

    removing ex005

--- ex005.py
+++
@@ -1,3 +0,0 @@
-# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
-num = int(input('Digite um número: '))
-print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}.'.format(num, num - 1, num +1))
