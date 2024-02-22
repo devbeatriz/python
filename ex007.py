de04e56932d06353b9ebda248a3d744d3920df28
Beatriz Rodrigues <beatrizrodrrigues@gmail.com>
2024-02-22 20:40:27 -0300

    Média aritmética

---
+++ ex007.py
@@ -0,0 +1,4 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+print('A média do aluno é de {:.1f}'.format((n1 + n2) /2))
