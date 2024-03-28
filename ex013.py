f8c2fde4318a698bd3f4d61feee2fe479921c6fe
Beatriz Rodrigues <beatrizrodrrigues@gmail.com>
2024-03-28 00:49:31 -0300

    Reajuste salarial

---
+++ ex013.py
@@ -0,0 +1,4 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+salario = float(input('Qual é o salário do funcionário? R$'))
+novo_salario = salario + (salario*15/100)
+print('Um funcionário que ganhava R${:.2f}, com o aumento de 15% passa a receber R${:.2f}'.format(salario,novo_salario))
